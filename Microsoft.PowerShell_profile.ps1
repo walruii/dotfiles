e2ccedad789b7e614fb4e5035c0220f41cfe0d56
walruii <walruiix@gmail.com>
2026-04-04 10:34:25 +0530

    updated PowerShell config to add some unix commands

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,19 @@ function cd {
         z @args
     }
 }
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -ViModeIndicator Cursor
+Set-PSReadLineKeyHandler -Key "Shift+Tab" -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord "Ctrl+n" -Function NextHistory
+Set-PSReadLineKeyHandler -Chord "Ctrl+p" -Function PreviousHistory
